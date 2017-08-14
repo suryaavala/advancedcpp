006ca0c55d3b65689e6d6171338a080be94131c0
Surya Avala <suryaavala.dev@gmail.com>
2017-08-14 21:55:08 +1000

    Calculator init

---
+++ assignments/a1/calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+int main(int argc, char* argv[]) {
+
+	// setup the print out format for the precision required.
+	std::cout.setf(std::ios::fixed,std::ios::floatfield);
+	std::cout.precision(3);
+
+	// open the file for reading
+	std::ifstream in;
+	in.open(argv[1]);
+
+	// string to be read into
+	std::string s;
+
+	// read the file while we have input.
+	while (in >> s) {
+
+	}
+	in.close();
+}
