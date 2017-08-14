bcbc2bc37a2fda39c994e9340717d8c10bfaa34d
Surya Avala <suryaavala.dev@gmail.com>
2017-08-14 22:53:51 +1000

    stack implemented for add sub mul

--- assignments/a1/calculator.cpp
+++ assignments/a1/calculator.cpp
@@ -2,6 +2,17 @@
 #include <fstream>
 #include <iomanip>
 
+//include string and stack
+#include <string>
+#include <stack>
+
+//include cmath for sqrt
+#include <cmath>
+
+
+//TODO Delete Typinfo after debugging
+#include <typeinfo>
+
 int main(int argc, char* argv[]) {
 
 	// setup the print out format for the precision required.
@@ -15,9 +26,33 @@ int main(int argc, char* argv[]) {
 	// string to be read into
 	std::string s;
 
+  //  Claculator stack
+  std::stack <std::string> calc;
+  //TODO Stack functions
+  // empty() – Returns whether the stack is empty
+  // size() – Returns the size of the stack
+  // top() – Returns a reference to the top most element of the stack
+  // push(g) – Adds the element ‘g’ at the top of the stack
+  // pop() – Deletes the top most element of the stack
+
+
 	// read the file while we have input.
 	while (in >> s) {
 
+    //TODO Delete cout
+    std::cout << s << isdigit(s[0])<<  '\n';
+
+    if (isdigit(s[0])) {
+      calc.push(s);
+    }
+    else {
+      std::string a, b;
+      a = calc.top();
+      calc.pop();
+      b = calc.top();
+      calc.pop();
+      std::cout << a << " " << b << " " << s << '\n';
+    }
 	}
 	in.close();
 }
