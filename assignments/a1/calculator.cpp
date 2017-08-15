ab6648886d74c607e4369497fcfd2051c14948f5
Surya Avala <suryaavala.dev@gmail.com>
2017-08-15 23:22:05 +1000

    resolves #3, resolves #4

--- assignments/a1/calculator.cpp
+++ assignments/a1/calculator.cpp
@@ -6,25 +6,19 @@
 #include <string>
 #include <stack>
 
-//include queue for reversing
+//include queue for reversing the stack and capturing repeat calculations
 #include <queue>
 
 //include cmath for sqrt
 #include <cmath>
 
-//include string stream for double to string precision
+//include string stream for precision during double to string conversion
 #include <sstream>
 
 
-//TODO Delete Typinfo after debugging
-#include <typeinfo>
-void print_stack(std::stack<std::string> c);
-void print_queue(std::queue<std::string> c);
-bool DEBUG = 0;
-
-//Functions for calculations and repeat
+//Functions for calculations and repeat (explained during function definitions)
 std::stack <std::string> calculate (std::stack <std::string> current_stack, std::string operation);
-std::stack <std::string> enrepeat (std::stack <std::string> current_stack, std::queue <std::string> rep_stack);
+std::stack <std::string> enrepeat (std::stack <std::string> current_stack, std::queue <std::string> rep_queue);
 
 
 
@@ -41,57 +35,59 @@ int main(int argc, char* argv[]) {
 	// string to be read into
 	std::string s;
 
-  //  Claculator stack
+  //  Claculator stack, which stores the numbers(inputs and answers) to be operated on
   std::stack <std::string> calc;
 
-  //Repeat queue and repeat mode
+  //Repeat queue which stores the commands inputed between repeat and end repeat
   std::queue <std::string> rep;
+
+  //Boolean to check if we are in repeat mode or normal operational mode
+  //When in Repeat mode, all the inputs are stored and they are only executed after the "endrepeat" command
+  //Default non-repeat/operational mode
   bool rep_mode = 0;
-
-
-  //TODO Stack functions
-  // empty() – Returns whether the stack is empty
-  // size() – Returns the size of the stack
-  // top() – Returns a reference to the top most element of the stack
-  // push(g) – Adds the element ‘g’ at the top of the stack
-  // pop() – Deletes the top most element of the stack
 
 
 	// read the file while we have input.
 	while (in >> s) {
-                                        if(DEBUG)
-                                        {//TODO Delete cout
-                                        std::cout << s << " = s here "<<  '\n';}
 
-    //Check for if s is a digit or starts with - or starts with "."
+    //Check for if s is a valid number and the program is not in repeat mode (repeat mode as defined in line 44)
+    //A number is valid when either:
+    //1. It is a digit, example: 1, 66, 190, 4.5
+    //2. Starts with "-", example: -1, -56, -1.1, -8.4  or
+    //3. Starts with ".", example: .1, .094
     if ((isdigit(s[0]) || !s.compare(0,1,"-") || !s.compare(0,1,".")) && !rep_mode) {
 
-      //IF s is a double then push it as string with proper precision
+      //If s is a double then push it to calc stack as string with proper precision, example: 16.0 will be pushed as 16.000
       if (s.find('.') != std::string::npos) {
         double n = std::stod(s);
+        //Precision is acheived with sstream manipulations
         std::stringstream s_stream;
         s_stream << std::fixed << std::setprecision(3) << n;
         s = s_stream.str();
       }
       calc.push(s);
     }
+    //If s neither repeat nor endrepeat and the program is not in repeat mode, then go ahead and perform the calculation
     else if (s != "repeat" && s != "endrepeat" && !rep_mode){
       calc = calculate(calc, s);
     }
+    //If s is repeat or the program is not in repeat mode and s is not endrepeat (to avoid coming here during endrepeat statement)
     else if (s == "repeat" || (rep_mode && s != "endrepeat")) {
+      //change the program to repeat mode
       rep_mode = 1;
 
-      //Capturing the commands into rep stack other the "repeat" command itself
+      //Push the commands (other than "repeat" command itself) into rep queue
       if (s != "repeat") {
         rep.push(s);
       }
     }
-    else if (s == "endrepeat") {
-                                              if(DEBUG)
-                                              {std::cout << "end repeat" << '\n';}
+    //If s is endrepeat the program is in repeat mode, then perform calculations where were being captured in rep queue as per the specs
+    //endrepeat is ignored if a repeat is not issued as one of the previous commands
+    else if (s == "endrepeat" && rep_mode) {
+
       calc = enrepeat(calc, rep);
 
-      //cleaning stack rep and rep_mode
+      //cleaning/emptying rep queue and changing the program to non-repeat mode
       rep = std::queue <std::string>();
       rep_mode = 0;
     }
@@ -99,44 +95,50 @@ int main(int argc, char* argv[]) {
 	in.close();
 }
 
-void print_stack(std::stack<std::string> c) {
-    while (!c.empty()) {
-        std::cout<<c.top()<<" ";
-        c.pop();
-    }
-    //std::cout << "\nreversed" << '\n';
-}
-
-void print_queue(std::queue<std::string> q) {
-    while (!q.empty()) {
-        std::cout<<q.front()<<" ";
-        q.pop();
-    }
-    //std::cout << "\nreversed" << '\n';
-}
-
+//calculate function which is called when the perform the following operations:
+// add, sub, mult, div, sqrt, pop, reverse
+//Takes the current stack as an input
+//Performs the requested operation
+//prints out the neccessary outputs
+//Modifies the stack as needed during the operations
+//And returns the modified stack
 std::stack <std::string> calculate (std::stack <std::string> current_stack, std::string operation) {
 
+  //commands which require two pops (two numbers) in order to be executed
   if (operation == "add" || operation == "sub" || operation == "mult" || operation == "div") {
+
+    //Popping the last two elements from the stack
+    //Stack throws a seg fault and the program will be terminated if these operations are performed on a stack with size less than 2
     std::string a, b;
     a = current_stack.top();
     current_stack.pop();
     b = current_stack.top();
     current_stack.pop();
 
-                                    if(DEBUG)
-                                    {std::cout << a << " " << b << " " << operation << '\n';}
+    //All these four operations are performed in the following order:
+    //1. If either of the numbers are doubles then
+      //1.1 initialise two temporary double variables by poping (& convert) last two elements on the stack
+      //1.2 perform the operation, which results in a double as an answer
+      //1.3 output the computation
+      //1.4 convert the ans from double to ans_str with a precision of 3
+      //1.5 push ans_str back to the stack
+    //2. If neither of the numbers are doubles then
+      //2.1 initialise two temporary int variables by poping (& converting) last two elements on the stack
+      //2.2 perform the operation, which results in an int as an answer
+      //2.3 output the computation
+      //2.4 convert int ans to srting and push it to stack
 
-    //Caclulation
+    //addition
     if (operation == "add") {
       if (a.find('.') != std::string::npos || b.find('.') != std::string::npos) {
           double n1 = std::stod(a);
           double n2 = std::stod(b);
           double ans = n1 + n2;
 
+          //Outputs are strings a & b instead of n1 and n2 as per the spec
           std::cout << a << " + " << b << " = " << ans << '\n';
 
-          //Double to string with precision(3) then push to stack
+          //Double to string with precision(3) then push answer back to stack
           std::stringstream ans_stream;
           ans_stream << std::fixed << std::setprecision(3) << ans;
           std::string ans_str = ans_stream.str();
@@ -153,6 +155,7 @@ std::stack <std::string> calculate (std::stack <std::string> current_stack, std:
       }
     }
 
+    //subtraction
     else if (operation == "sub") {
       if (a.find('.') != std::string::npos || b.find('.') != std::string::npos) {
           double n1 = std::stod(a);
@@ -179,6 +182,7 @@ std::stack <std::string> calculate (std::stack <std::string> current_stack, std:
       }
     }
 
+    //multiplication
     else if (operation == "mult") {
       if (a.find('.') != std::string::npos || b.find('.') != std::string::npos) {
           double n1 = std::stod(a);
@@ -205,6 +209,7 @@ std::stack <std::string> calculate (std::stack <std::string> current_stack, std:
       }
     }
 
+    //division
     else if (operation == "div") {
       if (a.find('.') != std::string::npos || b.find('.') != std::string::npos) {
           double n1 = std::stod(a);
@@ -231,17 +236,17 @@ std::stack <std::string> calculate (std::stack <std::string> current_stack, std:
       }
     }
 
-
-
   }
 
+  //Square root, requires only one pop
   else if (operation == "sqrt") {
+    //Pop the most recent element
+    //If the stack is not of appropriate size, then a seg fault is thrown as explained in line 111
     std::string a;
     a = current_stack.top();
     current_stack.pop();
-                                                if(DEBUG)
-                                                {std::cout << a << " " << operation << '\n';}
 
+    //checks if the popped element is a double or int and performs the computation, prints and stores the result accordingly (similiar to how addition and others are done)
     if (a.find('.') != std::string::npos) {
 
       double n1 = std::stod(a);
@@ -267,17 +272,21 @@ std::stack <std::string> calculate (std::stack <std::string> current_stack, std:
   }
 
   //Pop
+  //Deletes the most recent element from the stack
   else if (operation == "pop") {
     current_stack.pop();
   }
 
   //Reverse stack
-  else if (operation == "reverse") {
-                                if(DEBUG)
-                                {  std::cout << "Current Stack before reversing: " << '\n';
-                                  print_stack(current_stack);
-                                  std::cout << "/* Reversing */" << '\n';}
+  //1.1 Pops the most recent element from the stack, lets call it "elements_reversed"
+  //1.2 Reverse the most recent "elements_reversed" number of elements in the stack
+  //During reversal poped elements are stored in a temporary queue
+  //Example: stack = 1,2,3,4,5 and elements_reversed = 2
+  //Then 5,4 are removed from the stack in that order and stored in tempcalc = 5,4; stack = 1,2,3
+  //Elements in tempcalc (from the front) are added back in the stack in that order
+  //Then stack becomes 1,2,3,5,4 (reversing the last two elements)
 
+  else if (operation == "reverse") {
 
     int elements_reversed = std::stoi(current_stack.top());
     current_stack.pop();
@@ -288,49 +297,50 @@ std::stack <std::string> calculate (std::stack <std::string> current_stack, std:
       current_stack.pop();
       elements_reversed -= 1;
     }
-                                if (DEBUG)
-                                {//current_stack = tmpcalc;
-                                std::cout << "Temp queue while reversing: " << '\n';
-                                print_queue(tmpcalc);}
 
     while (!tmpcalc.empty()) {
       current_stack.push(tmpcalc.front());
       tmpcalc.pop();
     }
-                                  if (DEBUG)
-                                  {    std::cout << "Current Stack after reversing: " << '\n';
-                                      print_stack(current_stack);
-                                      std::cout << "/* message */" << '\n';}
+
   }
 
+  //function returns the modified stack back
   return current_stack;
 }
 
-std::stack <std::string> enrepeat (std::stack <std::string> current_stack, std::queue <std::string> rep_stack){
+//enrepeat function which is called by main when it sees endrepeat command
+//enrepeat takes the calc stack and rep_queue as its arguments
+  //calc stack is the Claculator stack, which has stored the numbers(inputs and answers) to be operated on
+  //rep queue is the Repeat queue which has stored the commands/numbers inputed between repeat and end repeat
+//executes(by calling calcute fucntions)/repeats the commands on the repeat queue as desired/per spec
+//modifies the current stack accordingly
+//and returns the modified stack in the end
+std::stack <std::string> enrepeat (std::stack <std::string> current_stack, std::queue <std::string> rep_queue){
 
-                                  if(DEBUG)
-                                  {std::cout << "/* Printing rep queue */" << '\n';
-                                  print_queue(rep_stack);
-                                  std::cout << "/* message */" << '\n';}
+  //Removes the most recent number on the calculator stack and stores in nb_repeats
+  //nb_repeats is the number of times the commands on the repeat queue are executed
   int nb_repeats = std::stoi(current_stack.top());
   current_stack.pop();
 
   while (nb_repeats) {
-    std::queue <std::string> temp_rep_stack = rep_stack;
+    //Repeat commands from rep_queue are temporarily copied to temp_rep_queue
+    std::queue <std::string> temp_rep_queue = rep_queue;
 
     //Rep stack for nested repeats
+    //Nested repeats are executed recursively
     std::queue <std::string> nested_rep;
     bool nested_rep_mode = 0;
-    while (!temp_rep_stack.empty()) {
 
-      std::string s = temp_rep_stack.front();
-                                          if(DEBUG)
-                                          {//TODO Delete cout
-                                          std::cout << s << " = s here "<<  '\n';}
+    //Execute commands from the front of temp_rep_queue until it is exhausted
+    while (!temp_rep_queue.empty()) {
 
+      std::string s = temp_rep_queue.front();
+
+      //similar to what main has done with the inputs from the file, except here we are considering the inputs from the repeats queue as commands
       if ((isdigit(s[0]) || !s.compare(0,1,"-") || !s.compare(0,1,"."))  && !nested_rep_mode) {
 
-        //IF s is a double then push it as string with proper precision
+        //If s is a double then push it as string with proper precision
         if (s.find('.') != std::string::npos) {
           double n = std::stod(s);
           std::stringstream s_stream;
@@ -339,15 +349,18 @@ std::stack <std::string> enrepeat (std::stack <std::string> current_stack, std::
         }
         current_stack.push(s);
       }
+      //if the commands are non repeats then they are executed by calling calculate function
       else if (s != "repeat" && s != "endrepeat" && !nested_rep_mode){
         current_stack = calculate(current_stack, s);
       }
+      //if this function sees another repeat, then it goes into nested repeat mode recursively
       else if (s == "repeat" || (nested_rep_mode && s != "endrepeat")) {
         nested_rep_mode = 1;
         nested_rep.push(s);
 
       }
       else if (s == "endrepeat") {
+        //recursively executes nested repeats
         current_stack = enrepeat(current_stack, nested_rep);
 
         //Cleaning nested_rep and nested_rep_mode
@@ -355,11 +368,13 @@ std::stack <std::string> enrepeat (std::stack <std::string> current_stack, std::
         nested_rep_mode = 0;
       }
 
-      temp_rep_stack.pop();
+      temp_rep_queue.pop();
     }
 
-    //reducing the number of repeats
+    //reducing the number of repeats every time all the commands on repeat queue are executed
     nb_repeats -= 1;
   }
+
+  //returns the modified stack
   return current_stack;
 }
